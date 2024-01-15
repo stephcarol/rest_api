4c0476cea3959db07ceee39104db69055737e723
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 <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>
2024-01-14 18:48:13 -1200

    subiendo!

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/suppliers', suppliersRouter);
+
+
 /* CONFIGURACIÓN DE LA RUTA A LA DOCUMENTACIÓN */
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
